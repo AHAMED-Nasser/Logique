93161d4935bb3ac18ba92b7647c8ff4e9f4b5bbf
nularyNasser <nahamed.sveil@gmail.com>
2026-05-17 13:52:42 +0200

    test dans main

--- Workdir/main.cxx
+++ Workdir/main.cxx
@@ -8,16 +8,26 @@ using namespace std;
 int main() {
 
     Formule f;
-    f.setFormule("(a|b)&c");
+    f.setFormule("-((p>q)&(r>s))");
 
-    string implement_free = f.impl_free();
-    cout << "VRAI : " << impl_free("(a>b)") << '\n';
-    cout << "VRAI : " << impl_free("(a>(b>c))") << '\n';
-    cout << "VRAI : " << impl_free("-(a>b)") << '\n';
-    cout << "FAUX : " << impl_free("((p>q)&(r>s))") << '\n';
-    cout << "VRAI : " << impl_free("-(p>q)&(r>s)") << '\n';
-    cout << "FAUX : " << impl_free("-((p>q)&(r>s))") << '\n';
-    cout << "FAUX : " << impl_free("((p>q)>(r>s))") << '\n';
-    
+    cout << "Formule de base : " << f.toString() << '\n';
+    cout << "Implication : " << f.impl_free() << '\n';
+    cout << "Morgan : " << f.morgan() << '\n';
+
+    // string implement_free = f.impl_free();
+    // cout << "VRAI : " << impl_free("(a>b)") << '\n';
+    // cout << "VRAI : " << impl_free("(a>(b>c))") << '\n';
+    // cout << "VRAI : " << impl_free("-(a>b)") << '\n';
+    // cout << "FAUX : " << impl_free("((p>q)&(r>s))") << '\n';
+    // cout << "VRAI : " << impl_free("-(p>q)&(r>s)") << '\n';
+    // cout << "FAUX : " << impl_free("-((p>q)&(r>s))") << '\n';
+    // cout << "FAUX : " << impl_free("((p>q)>(r>s))") << '\n';
+
+    // cout << "VRAI : " << morgan("(-a|b)") << '\n';
+    // cout << "VRAI : " << morgan("-(-a|b)") << '\n';
+    // cout << "VRAI : " << morgan("-(-p|q)&(-r|s)") << '\n';
+    // cout << "VRAI : " << morgan("-((-p|q)&(-r|s))") << '\n';
+    // cout << "VRAI : " << morgan("-((-p|q)|(-r|s))") << '\n';
+
     return 0;
 }
